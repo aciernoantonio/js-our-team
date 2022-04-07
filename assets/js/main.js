bc8343a777303e46a26c17646a4abd88ce0ba4fc
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-07 16:07:30 +0200

    add display infos js section

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,5 +48,26 @@ for (let i = 0; i < team.length; i++){
 }
 
 
+for (let i = 0; i < team.length; i++){
+
+    //seleziono la posizione degli elementi presenti nell'array
+    let info = team[i];
+
+    //seleziono l'elemento della dom
+    let listGroup = document.querySelector(".listgroup");
+    
+    //creo una sottolista per ogni oggetto dell'array
+
+
+    //creo il list item
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${info.name}<br>\n ${info.role}<br>\n ${info.image}`;
+
+    //passo i list item nel listgroup
+    listGroup.append(listItem);
+
+}
+
+
 
 
